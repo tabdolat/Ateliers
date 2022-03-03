e6533a0ec8cf8e9f1366bea35e914704131e1d83
BOZ Atman <aboz@bordeaux-inp.fr>
2022-03-03 18:18:20 +0100

    atman pg1819

---
+++ atman/ntree.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "ntree.h"
+
+
+struct ntree_t * ntree__empty(void){
+    struct ntree_t *t=malloc(sizeof( struct ntree_t ));
+    t->LEFT=malloc(size)
+}
+
+int ntree__is_empty(struct ntree_t const * t){
+    return (t==NULL);
+}
+
+void ntree__free(struct ntree_t * t){
+
+}
